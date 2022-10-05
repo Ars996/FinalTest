478e3454b72e838d194a91d0f16853651d3f3c76
Arseniy <ars-klepikov@yandex.ru>
2022-10-05 11:59:22 +0700

    программа написана

--- FinalTest/Program.cs
+++ FinalTest/Program.cs
@@ -3,5 +3,41 @@
 //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
 //коллекциями, лучше обойтись исключительно массивами.
 
-int [] arr = ['Ghb',2, 2,]
-if fgr
+ using System;
+using static System.Console; 
+
+Clear();
+
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] AskArray() {
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
